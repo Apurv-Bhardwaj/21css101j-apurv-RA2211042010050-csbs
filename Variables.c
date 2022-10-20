9491fcdc8c378edac5c6088b70130b74cc56a4af
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-21 03:20:22 +0530

    Create Variables.c

---
+++ Variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() 
+{
+  int age;
+  age = 17;
+  int birthyear;
+  birthyear = 2004; 
+  int example;
+  example = birthyear - age;
+  printf("i am %d years old and i was born in %d and i am planning on buying %d snacks", age, birthyear, example);
+}
