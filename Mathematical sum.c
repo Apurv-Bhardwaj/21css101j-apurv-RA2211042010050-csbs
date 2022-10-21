1f319e43f0cc11ba8a4d13819cc40d5bf2047fce
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-21 11:51:53 +0530

    Create Mathematical sum.c

---
+++ Mathematical sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+  char main[6] = "apurv";
+  int myage;
+  myage = 24;
+  int girlsage = (myage / 2) + 7;
+  printf ("mark can date a girl of %d age and he is %d", girlsage, myage);
+  /*here as it shows the age of girls that mark is capable of dating has been calculated to 19
+  and he is 24 years old*/
+  return 0;
+}
