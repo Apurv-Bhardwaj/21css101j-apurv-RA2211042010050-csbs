df47d7b646258badf7bfadc862ef7920ecaecb4a
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-21 11:23:30 +0530

    Create Strcpy.c

---
+++ Strcpy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+ char name[] = "Apurv Bhardwaj";
+printf("my name is %s\n", name);
+
+ strcpy(name , "aditi bhardwaj");
+ printf("my name is %s", name);
+
+ return 0;
+}
