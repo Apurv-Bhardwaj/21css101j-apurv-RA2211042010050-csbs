934c8a51a681d0bff7b0afcd0c02812374625729
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-26 09:49:31 +0530

    Create learning for loop.c
    using for loop function to create a matrix of numbers

---
+++ learning for loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+ int i,j,k;
+ for(i=1;i<=4;i++)
+ {j=i+1;
+ k=j+1;
+ printf("%d\t%d\t%d\n",i,j,k);}
+}
+//this program successfully prints the intended 3x3 matrix of the numbers coded into the software
