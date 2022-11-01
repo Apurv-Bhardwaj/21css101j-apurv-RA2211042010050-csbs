582c31c375b1d721974e013c9959197c9b6e81cd
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-01 20:34:03 +0530

    Create getting input with scanf.c

---
+++ getting input with scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main() 
+{
+ char name[20];
+ char college[20];
+ int trophies[20];
+  
+  printf("hi what is your name\n");
+  scanf("%s", name);
+  
+  printf("what college do you go in\n");
+  scanf("%s", college);
+  
+  printf("how many trophies have you won\n");
+  scanf("%d", trophies);
+  return 0;
+}
+  
