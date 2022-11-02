c3880fc59f4d1e08f7df25410280ad25def0df2f
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-03 02:40:57 +0530

    Update using an if statement to find if a person is eligible to vote.c

--- using an if statement to find if a person is eligible to vote.c
+++ using an if statement to find if a person is eligible to vote.c
@@ -6,8 +6,11 @@ int main() {
   printf("how old are you?\n");
   scanf("%d", &age);
 
-  if(age > 18){
+  if(age >= 18){
       printf("you are elgible to vote!");
+  }
+  if(age < 18) {
+      printf("you are not eligible to vote.");
   }
 } 
     
