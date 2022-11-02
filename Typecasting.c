79a0970f925adc286d122a4a542dcde44ebe17b6
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-03 00:57:47 +0530

    Create Typecasting.c

---
+++ Typecasting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    //typecasting a method to convert int variables to float and other on between the code
+    //here we will be calculating the amount of profit (dollars) that a bakery generates on its bagels on a dialy basis
+    float avgprofit;
+    int bagelssold = 45;
+    int daysworked = 20;
+    int price = 23;
+    
+    avgprofit = (float) price * (float) bagelssold / (float) daysworked; //here the variables are converted to float for a more accurate response
+    printf("The daily profit on bagels is: $%.2f", avgprofit);
+       
+} 
+    
+    
