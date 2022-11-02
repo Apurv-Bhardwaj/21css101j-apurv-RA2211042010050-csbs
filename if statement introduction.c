670bbaea85cee8aba4e6817e27755912433fb109
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-03 02:21:24 +0530

    Create if statement introduction.c

---
+++ if statement introduction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+   //using the if statement
+   //basic structure - if(test) { printf("this will print if (test) is true"); } 
+   
+   if(5 == 5) {
+       printf("correct");
+   }
+} 
+    
+    
