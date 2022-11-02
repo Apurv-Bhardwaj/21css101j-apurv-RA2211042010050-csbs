99a1cf02d339a313c4fd03a931a17bf4a8c0c9e9
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-03 00:31:52 +0530

    Create creating a continuous counting code.c

---
+++ creating a continuous counting code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int pageviews = 0;
+    
+    pageviews = pageviews + 1;
+    printf ("The number of page views: %d \n", pageviews);
+    pageviews = pageviews + 1;
+    printf ("The number of page views: %d \n", pageviews);
+   //Here it counts the pageviews depending upon the amount of times its printed 
+} 
