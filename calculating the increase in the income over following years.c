4e87fb118c7ca40733848f38b8471c291832001a
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-03 00:36:37 +0530

    Create calculating the increase in the income over following years.c

---
+++ calculating the increase in the income over following years.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    float balance = 100.00;
+    balance *= 1.2;
+    printf("my earning in the subsequent year will be: %f\n", balance);
+    balance *= 1.2;
+    printf("my earning in the subsequent year will be: %f\n", balance);
+    balance *= 1.2;
+    //here the code will proceed to tell me the amount of income i made over the span of three years with consideration to the yearly increment of 1.2
+} 
+    
+    
