4013055a57f06bf8508a49c729fdc1bca3e437f0
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-14 09:57:55 +0530

    Update calculating the increase in the income over following years.c

--- calculating the increase in the income over following years.c
+++ calculating the increase in the income over following years.c
@@ -9,5 +9,6 @@ int main()
     balance *= 1.2;
     //here the code will proceed to tell me the amount of income i made over the span of three years with consideration to the yearly increment of 1.2
 } 
+regitsration number: RA2211042010050
     
     
