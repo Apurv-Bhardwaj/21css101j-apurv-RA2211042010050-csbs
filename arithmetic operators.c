48a212c99279c735e8d48e34204c56c45e1a1c6c
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-20 00:03:46 +0530

    Create arithmetic operators.c

---
+++ arithmetic operators.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int  num1 = 5, num2 = 10;
+  printf("Addition Result = %d\n",(num1 + num2));
+  printf("Subtraction Result = %d\n",(num1 - num2));
+  printf("Multiplication Result = %d\n",(num1 & num2));
+  printf("Division Result = %d\n",(num1 / num2));
+  printf("RA2211042010050");
+}
