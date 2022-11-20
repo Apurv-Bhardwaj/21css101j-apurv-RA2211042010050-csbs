52731cf2a724ae55a83bbfe68bec89bae469bf84
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-21 04:56:50 +0530

    Create program to find simple interest.c

---
+++ program to find simple interest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+printf("RA2211042010050");
+int p,r,t,si;
+printf(“enter principle, Rate of interest & time to find simple interest: ”);
+scanf(“%d%d%d”,&p,&r,&t);
+si=(p*r*t)/100;
+printf(“simple intrest= %d”,si);
+}
