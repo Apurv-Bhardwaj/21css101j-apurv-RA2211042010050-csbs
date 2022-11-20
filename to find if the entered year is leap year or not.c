0e6e272c41dddd6721b3f686f274cdd8808eab86
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-21 05:12:05 +0530

    Create to find if the entered year is leap year or not.c

---
+++ to find if the entered year is leap year or not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+printf("RA2211042010050");
+int n;
+printf(“enter any year: ”);
+scanf(“%d”,&n);
+if(n%4==0)
+printf(“year is a leap year”);
+else
+printf(“year is not a leap year”);
+}
