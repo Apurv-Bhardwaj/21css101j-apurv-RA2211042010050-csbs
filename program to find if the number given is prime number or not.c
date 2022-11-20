ed9e01286ea314d52c979547048c2647e0401b81
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-21 05:15:04 +0530

    Create program to find if the number given is prime number or not.c

---
+++ program to find if the number given is prime number or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+printf("RA2211042010050");
+int i,n,r=0;
+printf(“Enter any no: ”);
+scanf(“%d”,&n);
+for(i=2;i<=n-1;i++)
+{
+if(n%i==0)
+r=1;
+break;
+}
+if(r==0)
+printf(“prime no”);
+else
+printf(“Not prime”);
+}
