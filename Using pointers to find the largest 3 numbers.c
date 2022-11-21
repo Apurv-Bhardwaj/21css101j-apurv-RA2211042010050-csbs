404dcbf426703deb1ffc80e67d155b148e77e341
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-21 07:26:59 +0530

    Create Using pointers to find the largest 3 numbers.c

---
+++ Using pointers to find the largest 3 numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+printf("RA2211042010050");
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+printf("%d",a);
+else if(b>c)
+printf("%d",b);
+else
+printf("%d",c);
+return 0;
+}
