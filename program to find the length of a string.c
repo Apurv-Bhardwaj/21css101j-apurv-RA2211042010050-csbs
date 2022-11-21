0b85755ef9fd3896b6a22f147236f106a74d650c
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-21 06:52:48 +0530

    Create program to find the length of a string.c

---
+++ program to find the length of a string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+printf("RA2211042010050");
+char s[] = "Programming is fun";
+int i;
+for (i = 0; s[i] != '\0'; ++i);
+printf("Length of the string: %d", i);
+return 0;
+}
