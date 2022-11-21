1557b5ddc4237f90c1d69999d57aeda597e1f7d7
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-21 07:30:13 +0530

    Create Finding the number of characters present.c

---
+++ Finding the number of characters present.c
@@ -0,0 +1,17 @@
+include <stdio.h>
+#include <string.h>
+struct name
+{
+printf("RA2211042010050");
+char str[10];
+}s;
+int main()
+{
+int len;
+char str[10];
+scanf("%s",str);
+struct name;
+len=strlen(str);
+printf("%d",len);
+return 0;
+}
