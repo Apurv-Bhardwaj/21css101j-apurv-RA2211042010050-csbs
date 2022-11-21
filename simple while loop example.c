c298449783ab125abb81be6e97f5f080582b53b5
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-21 06:43:09 +0530

    Create simple while loop example.c

---
+++ simple while loop example.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+printf("RA2211042010050");
+printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+int i = 0;  // declaration and initialization at the same time
+printf("\nPrinting numbers using while loop from 0 to 9\n\n");
+while(i<10)
+{
+printf("%d\n",i);
+i++;    // same as i=i+1;
+}
+printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+return 0;
+}
