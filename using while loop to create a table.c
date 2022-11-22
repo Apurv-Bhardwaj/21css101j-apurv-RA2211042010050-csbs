4661aa17628ff8dba6e61fd2c1d785202b73d758
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-22 10:28:16 +0530

    Create using while loop to create a table.c

---
+++ using while loop to create a table.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+int main(){  
+printf("RA2211042010050");
+int i=1,number=0,b=9;    
+printf("Enter a number: ");    
+scanf("%d",&number);    
+while(i<=10){    
+printf("%d \n",(number*i));    
+i++;    
+}    
+return 0;  
+}   
