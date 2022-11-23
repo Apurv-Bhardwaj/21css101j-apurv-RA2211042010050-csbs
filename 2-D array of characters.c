61ca4fee253231ab081d237e060a924146047cd7
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-23 23:14:24 +0530

    Create 2-D array of characters.c

---
+++ 2-D array of characters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main()
+{
+char pet [2][5] = {"Lion","Cat"};
+  int i,j;
+  for(i=0; i<2; i++)
+  {
+  printf("\n%d", (pet+i)+j);
+    j=0;
+    while (*(*(pet + i)j) != '\0')
+  }
+  printf("%c", *(*(pet + i) + j) );
+  j++;
+}
+}
+  }
