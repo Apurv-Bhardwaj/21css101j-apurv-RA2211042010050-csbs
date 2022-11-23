9c6d9dc69371e7663d9e7ce64b44034714dabc62
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-23 23:29:10 +0530

    Create strstr function.c

---
+++ strstr function.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str1[30], str2 [30];
+  char *found;
+  printf("Enter two strings:\n");
+  gets(str1);
+  gets(str2);
+  printf("string2 is found a tposition %d in string1\n", found-str1);
+}
