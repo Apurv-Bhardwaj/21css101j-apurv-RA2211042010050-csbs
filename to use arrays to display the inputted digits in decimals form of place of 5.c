8e7624a53def5c3b7da1a895454e2996618858c4
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-25 04:36:12 +0530

    Create to use arrays to display the inputted digits in decimals form of place of 5.c

---
+++ to use arrays to display the inputted digits in decimals form of place of 5.c
@@ -0,0 +1,8 @@
+#inlcude <stdio.h>
+void main() {
+printf("RA2211042010050\n")
+     static float arr[] = {1.2, 16, 34, 4.5, 45};
+     int i;
+     for (i = 0; i < 5; i++)
+     printf("%f", arr[i]);
+}
