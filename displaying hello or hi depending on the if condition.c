ef62ff78d2b004e8cc364603385fe271a4fbae9a
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-11-25 04:41:03 +0530

    Create displaying hello or hi depending on the if condition.c

---
+++ displaying hello or hi depending on the if condition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+printf("RA2211042010050");
+    int A, a;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &A, &a);
+    if(A > a) {
+      printf("Hello...!");
+    }
+    else {
+     printf("Hi.....!");
+    }
+}
